194dd312d7d16d15910232fc5681544ed75b2a3d
Christoph Hennemann <christoph.hennemann@gmail.com>
2018-07-08 07:15:16 +0200

    feat: polishing logging utility

--- logging/src/main/kotlin/de/chennemann/utils/logging/LoggingUtils.kt
+++ logging/src/main/kotlin/de/chennemann/utils/logging/LoggingUtils.kt
@@ -2,22 +2,14 @@ package de.chennemann.utils.logging
 
 import kotlin.reflect.full.companionObject
 import org.apache.logging.log4j.LogManager
-import org.apache.logging.log4j.Logger
+import org.apache.logging.log4j.kotlin.KotlinLogger
+import org.apache.logging.log4j.kotlin.loggerOf
 
 /**
  * [Lazy] delegate for retrieving a Slf4j [Logger] instance.
  *
  * Usage:  private val log by logger(javaClass)
  */
-fun logger(javaClass: Class<Any>): Lazy<Logger> = lazy {
-    LogManager.getLogger(unwrapCompanionClass(javaClass).name)
-}
-
-/**
- * Unwrap companion class to enclosing class given a Java class
- */
-internal fun <T : Any> unwrapCompanionClass(ofClass: Class<T>): Class<*> {
-    return ofClass.enclosingClass?.takeIf {
-        ofClass.enclosingClass.kotlin.companionObject?.java == ofClass
-    } ?: ofClass
+fun logger(javaClass: Class<Any>): Lazy<KotlinLogger> = lazy {
+    loggerOf(javaClass)
 }
